ebc6b13fc3ea1bc6288b7b14a8c7d36d78d94a84
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-10 15:31:58 +0700

    feat(main): implemented Match entity:

---
+++ src/main/java/com/spacier/entity/Match.java
@@ -0,0 +1,41 @@
+package com.spacier.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Tables")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Match {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "first_player", referencedColumnName = "id", nullable = false)
+  private Player firstPlayer;
+
+  @ManyToOne
+  @JoinColumn(name = "second_player", referencedColumnName = "id", nullable = false)
+  private Player secondPlayer;
+
+  @ManyToOne
+  @JoinColumn(name = "winner", referencedColumnName = "id", nullable = false)
+  private Player winner;
+
+  @Override
+  public String toString() {
+    return "Match{" +
+           "id=" + id +
+           ", firstPlayer=" + firstPlayer +
+           ", secondPlayer=" + secondPlayer +
+           ", winner=" + winner +
+           '}';
+  }
+}
