398a4282c8f20914638f007aa7e41ba7bcff5169
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-10 15:49:50 +0700

    feat(hibernate): implemented init sql script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO Players(name) values ('A. Agassi');
+INSERT INTO Players(name) values ('A. Tilden');
+INSERT INTO Players(name) values ('B. Borg');
+INSERT INTO Players(name) values ('B. Jean King');
+INSERT INTO Players(name) values ('P. Sampras');
+INSERT INTO Players(name) values ('R. Emerson');
+INSERT INTO Players(name) values ('R. Laver');
+
+INSERT INTO Matches (first_player, second_player, winner) VALUES (1, 2, 1);
+INSERT INTO Matches (first_player, second_player, winner) VALUES (3, 2, 2);
+INSERT INTO Matches (first_player, second_player, winner) VALUES (5, 3, 5);
+INSERT INTO Matches (first_player, second_player, winner) VALUES (6, 4, 4);
