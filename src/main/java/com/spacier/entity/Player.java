265cc64d259d7b932ea8df365b25f00816279e81
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-10 15:28:11 +0700

    feat(main): added PLayer entity

--- src/main/java/com/spacier/entity/Player.java
+++ src/main/java/com/spacier/entity/Player.java
@@ -1,2 +1,28 @@
-package com.spacier.entity;public class Player {
+package com.spacier.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Players")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "name", nullable = false, unique = true, length = 50)
+  private String name;
+
+  @Override
+  public String toString() {
+    return "Player {id=" + id + ", name=" + name + "}";
+  }
 }
