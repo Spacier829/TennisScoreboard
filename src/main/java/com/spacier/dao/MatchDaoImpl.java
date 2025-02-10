3840e93137878cd1bfa627e8c4bca846994ed3d5
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-10 16:38:49 +0700

    feat(dao): implemented add method for match

--- src/main/java/com/spacier/dao/MatchDaoImpl.java
+++ src/main/java/com/spacier/dao/MatchDaoImpl.java
@@ -1,2 +1,30 @@
-package com.spacier.dao;public class MatchDaoImpl {
+package com.spacier.dao;
+
+import com.spacier.entity.Match;
+import com.spacier.util.HibernateUtil;
+import lombok.Getter;
+import org.hibernate.Session;
+
+import java.util.Optional;
+
+public class MatchDaoImpl implements Dao<Match> {
+  @Getter
+  private static final MatchDaoImpl INSTANCE = new MatchDaoImpl();
+
+  @Override
+  public Match add(Match match) {
+    try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+      session.beginTransaction();
+      session.persist(match);
+      session.getTransaction().commit();
+      return match;
+    } catch (RuntimeException e) {
+      throw new RuntimeException("Failed to add match to the database");
+    }
+  }
+
+  @Override
+  public Optional<Match> findByName(String name) {
+    return Optional.empty();
+  }
 }
