4f60d131930948c0a2f158375120b37155cdce17
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-10 16:38:27 +0700

    refactor(match): added cascade:

---
+++ src/main/java/com/spacier/dao/MatchDaoImpl.java
@@ -0,0 +1,2 @@
+package com.spacier.dao;public class MatchDaoImpl {
+}
--- src/main/java/com/spacier/entity/Match.java
+++ src/main/java/com/spacier/entity/Match.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cascade;
 
 @Entity
 @Table(name = "Matches")
@@ -18,11 +19,13 @@ public class Match {
   private Long id;
 
   @ManyToOne
+  @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
   @JoinColumn(name = "first_player", referencedColumnName = "id", nullable = false)
   private Player firstPlayer;
 
   @ManyToOne
   @JoinColumn(name = "second_player", referencedColumnName = "id", nullable = false)
+  @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
   private Player secondPlayer;
 
   @ManyToOne
