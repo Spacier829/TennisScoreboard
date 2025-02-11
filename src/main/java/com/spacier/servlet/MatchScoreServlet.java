106fc1c8bb0b449f31d02fe203a1e8cc83126a57
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-11 14:41:57 +0700

    feat(matchScoreServlet): implemented get method for render player names

--- src/main/java/com/spacier/servlet/MatchScoreServlet.java
+++ src/main/java/com/spacier/servlet/MatchScoreServlet.java
@@ -1,2 +1,32 @@
-package com.spacier.servlet;public class MatchScoreServlet {
+package com.spacier.servlet;
+
+import com.spacier.dto.MatchScoreDto;
+import com.spacier.service.OngoingMatchService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/match-score")
+public class MatchScoreServlet extends HttpServlet {
+  private final OngoingMatchService ongoingMatchService = OngoingMatchService.getINSTANCE();
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    UUID uuid = UUID.fromString(req.getParameter("uuid"));
+    MatchScoreDto ongoingMatch = ongoingMatchService.getMatch(uuid);
+
+    req.setAttribute("ongoingMatch", ongoingMatch);
+
+    req.getRequestDispatcher("match-score.jsp").forward(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+  }
 }
