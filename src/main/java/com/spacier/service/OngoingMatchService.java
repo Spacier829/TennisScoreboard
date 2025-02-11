c2703c5268d0973bc83aa5543c97979a8dbfc236
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-11 14:13:23 +0700

    feat(ongoingMAtchService): implemented service for process ongoing mathes

--- src/main/java/com/spacier/service/OngoingMatchService.java
+++ src/main/java/com/spacier/service/OngoingMatchService.java
@@ -1,2 +1,30 @@
-package com.spacier.service;public class OngoingMatchService {
+package com.spacier.service;
+
+import com.spacier.dto.MatchScoreDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@NoArgsConstructor
+public class OngoingMatchService {
+  @Getter
+  private static final OngoingMatchService INSTANCE = new OngoingMatchService();
+
+  private static final Map<UUID, MatchScoreDto> ongoingMatches = new HashMap<>();
+
+  public MatchScoreDto getMatch(UUID uuid) {
+    return ongoingMatches.get(uuid);
+  }
+
+  public UUID addMatch(MatchScoreDto matchScoreDto) {
+    ongoingMatches.put(matchScoreDto.getUuid(), matchScoreDto);
+    return matchScoreDto.getUuid();
+  }
+
+  public void removeMatch(UUID uuid) {
+    ongoingMatches.remove(uuid);
+  }
 }
