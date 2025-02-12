274d8bc7529205bbd6e74ddb080f1baac10938ed
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-12 12:58:06 +0700

    feat(MatchScoreCalculation): implemented base of method for calculation of score

--- src/main/java/com/spacier/service/MatchScoreCalculationService.java
+++ src/main/java/com/spacier/service/MatchScoreCalculationService.java
@@ -1,5 +1,15 @@
 package com.spacier.service;
 
-public class MatchScoreCalculationService {
+import com.spacier.dto.MatchScoreDto;
 
+public class MatchScoreCalculationService {
+  private final MatchScoreDto match;
+
+  public MatchScoreCalculationService(MatchScoreDto match) {
+    this.match = match;
+  }
+
+  public void calculatePlayerScore(String playerName) {
+    match.getPlayerByName(playerName).setPoints(match.getPlayerByName(playerName).getPoints() + 1);
+  }
 }
