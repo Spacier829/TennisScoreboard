45bfbad6c431cb53af17caa8ebcf0e1b9804c136
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-14 17:01:06 +0700

    feat(mathes servlet): added get method to view mathes

--- src/main/java/com/spacier/servlet/MatchesServlet.java
+++ src/main/java/com/spacier/servlet/MatchesServlet.java
@@ -1,5 +1,7 @@
 package com.spacier.servlet;
 
+import com.spacier.dao.MatchDaoImpl;
+import com.spacier.entity.Match;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,11 +9,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/matches")
 public class MatchesServlet extends HttpServlet {
+  private static final MatchDaoImpl matchDao = MatchDaoImpl.getInstance();
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    List<Match> matches = matchDao.findAll();
+    req.setAttribute("matches", matches);
     req.getRequestDispatcher("matches.jsp").forward(req, resp);
   }
 
