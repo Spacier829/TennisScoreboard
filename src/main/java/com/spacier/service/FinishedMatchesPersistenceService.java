64c337d90c3e2986c6afa1397c79bc527ff51e7b
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-14 15:55:57 +0700

    feat(finished match persistence): added methof for saving players

--- src/main/java/com/spacier/service/FinishedMatchesPersistenceService.java
+++ src/main/java/com/spacier/service/FinishedMatchesPersistenceService.java
@@ -1,32 +1,34 @@
 package com.spacier.service;
 
 import com.spacier.dao.MatchDaoImpl;
+import com.spacier.dao.PlayerDaoImpl;
 import com.spacier.dto.MatchScoreDto;
+import com.spacier.dto.PlayerScoreDto;
 import com.spacier.entity.Match;
 import com.spacier.entity.Player;
 
 public class FinishedMatchesPersistenceService {
   private static final FinishedMatchesPersistenceService INSTANCE = new FinishedMatchesPersistenceService();
   private static final MatchDaoImpl matchDao = MatchDaoImpl.getInstance();
+  private static final PlayerDaoImpl playerDao = PlayerDaoImpl.getInstance();
 
   public static FinishedMatchesPersistenceService getInstance() {
     return INSTANCE;
   }
 
-  public void save(MatchScoreDto match, String winnerName) {
-    Player firstPlayer = Player
-        .builder()
-        .name(match.getFirstPlayer().getName())
-        .build();
-    Player secondPlayer = Player.builder()
-        .name(match.getSecondPlayer().getName())
-        .build();
+  public void saveMatch(MatchScoreDto match, String winnerName) {
+    Player firstPlayer = playerDao.findByName(match.getFirstPlayer().getName()).orElseGet(
+        () -> savePlayer(match.getFirstPlayer()));
+    Player secondPlayer = playerDao.findByName(match.getSecondPlayer().getName()).orElseGet(
+        () -> savePlayer(match.getSecondPlayer()));
+
     Player winner;
     if (winnerName.equals(firstPlayer.getName())) {
       winner = firstPlayer;
     } else {
       winner = secondPlayer;
     }
+
     Match finishedMatch = Match
         .builder()
         .firstPlayer(firstPlayer)
@@ -34,5 +36,13 @@ public class FinishedMatchesPersistenceService {
         .winner(winner).build();
 
     matchDao.add(finishedMatch);
+  }
+
+  public Player savePlayer(PlayerScoreDto playerDto) {
+    Player player = Player
+        .builder()
+        .name(playerDto.getName())
+        .build();
+    return playerDao.add(player);
   }
 }
