5d5922e10c49857d10a35dca8a198661a18738ff
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-15 14:47:24 +0700

    feat(match dao): added methods for finding by name and count'

--- src/main/java/com/spacier/dao/MatchDaoImpl.java
+++ src/main/java/com/spacier/dao/MatchDaoImpl.java
@@ -10,6 +10,13 @@ import java.util.Optional;
 
 public class MatchDaoImpl implements Dao<Match> {
   private static final MatchDaoImpl INSTANCE = new MatchDaoImpl();
+  private static final String SELECT_ALL_HQL = "from Match";
+  private static final String COUNT_HQL = "SELECT COUNT(m) from Match m";
+  private static final String SELECT_BY_NAME_HQL = "FROM Match m WHERE m.firstPlayer.name = " +
+                                                   ":name " +
+                                                   "OR m" +
+                                                   ".secondPlayer.name = :name";
+  private static final String COUNT_BY_NAME_HQL = "SELECT COUNT(m)" + SELECT_BY_NAME_HQL;
 
   public static MatchDaoImpl getInstance() {
     return INSTANCE;
@@ -32,9 +39,13 @@ public class MatchDaoImpl implements Dao<Match> {
     return Optional.empty();
   }
 
-  public List<Match> findAll() {
+  public List<Match> findByNameWithPagination(String name, int page, int size) {
     try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-      return session.createQuery("from Match", Match.class).getResultList();
+      return session.createQuery(SELECT_BY_NAME_HQL, Match.class)
+          .setFirstResult((page - 1) * size)
+          .setParameter("name", name)
+          .setMaxResults(size)
+          .list();
     } catch (DataBaseException e) {
       throw new DataBaseException("Failed to find matches from the database");
     }
@@ -42,16 +53,26 @@ public class MatchDaoImpl implements Dao<Match> {
 
   public List<Match> findAllWithPagination(int page, int size) {
     try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-      return session.createQuery("from Match", Match.class).setFirstResult((page - 1) * size).setMaxResults(
+      return session.createQuery(SELECT_ALL_HQL, Match.class).setFirstResult((page - 1) * size).setMaxResults(
           size).list();
     } catch (DataBaseException e) {
       throw new DataBaseException("Failed to find matches from the database");
     }
   }
 
-  public long getCountMatches(){
+  public long getCountMatches() {
     try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-      return session.createQuery("SELECT COUNT(m) from Match m", Long.class).uniqueResult();
+      return session.createQuery(COUNT_HQL, Long.class).uniqueResult();
+    } catch (DataBaseException e) {
+      throw new DataBaseException("Failed to find matches from the database");
+    }
+  }
+
+  public long getCountMatchesByName(String name) {
+    try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+      return session.createQuery(
+              COUNT_BY_NAME_HQL, Long.class)
+          .setParameter("name", name).uniqueResult();
     } catch (DataBaseException e) {
       throw new DataBaseException("Failed to find matches from the database");
     }
