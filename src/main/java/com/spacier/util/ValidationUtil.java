c702759a6ef8b78a6a91d44088259425e8acdbf9
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-15 14:45:33 +0700

    feat(validation): added validation of pageNumber

--- src/main/java/com/spacier/util/ValidationUtil.java
+++ src/main/java/com/spacier/util/ValidationUtil.java
@@ -11,12 +11,28 @@ public class ValidationUtil {
     }
   }
 
-  private static void validateName(String name) {
+  public static void validateName(String name) {
     if (name.isEmpty() || name.isBlank()) {
       throw new InvalidParameterException("Name cannot be empty");
     }
     if (!name.matches("^[A-Z]\\. [A-Za-z]{1,98}$")) {
       throw new InvalidParameterException("Name must match the example: J. Doe");
     }
+  }
+
+  public static int validatePage(String pageNumber, long totalPages) {
+    int page;
+    try {
+      page = Integer.parseInt(pageNumber);
+    } catch (NumberFormatException exception) {
+      throw new InvalidParameterException("Invalid page number");
+    }
+    if (page < 0) {
+      page *= -1;
+    }
+    if (page > totalPages) {
+      page = (int) totalPages;
+    }
+    return page;
   }
 }
