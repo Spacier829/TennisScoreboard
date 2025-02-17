582a14af7bb5aca8e731ac1c2c9eb98e185f85e2
Spacier829 <mrsimpleeegor@gmail.com>
2025-02-17 11:15:07 +0700

    refactor(playerScoreDto): returned values to zero

--- src/main/java/com/spacier/dto/PlayerScoreDto.java
+++ src/main/java/com/spacier/dto/PlayerScoreDto.java
@@ -9,9 +9,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PlayerScoreDto {
   private String name;
-  private int points = 3;
-  private int games = 5;
-  private int sets = 1;
+  private int points = 0;
+  private int games = 0;
+  private int sets = 0;
 
   public PlayerScoreDto(String name) {
     this.name = name;
